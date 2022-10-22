46a87a5e605fed8776e2afa1bee50ea9bc83de1b
Şule <sule.kaptan@bil.omu.edu.tr>
2022-10-23 01:32:41 +0300

    added memesData

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,23 @@
-import React from "react"
+import React from 'react';
+import memesData from "../memesData"
 
-export default function Meme(  ) {
+export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+    
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+        
+    }
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input className="form-input" placeholder="Top text" type="text" />
                 <input className="form-input" placeholder="Bottom text" type="text" />
-                <button className="form-btn">Get a new meme image 🖼</button>
-            </form>
+                <button className="form-btn" onClick={getMemeImage}>Get a new meme image 🖼</button>
+            </div>
+            <img className="meme-img" src={memeImage}/>
         </main>
     )
 }
